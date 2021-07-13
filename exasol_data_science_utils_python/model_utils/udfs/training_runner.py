ca5f38cda7e0858c4822220dc381fb4b18f1a241
Torsten Kilias <torsten.kilias@exasol.com>
2021-07-13 22:31:00 +0200

    Add support for multiple groups to train_udf.py and training_runner.py - Models are now stored and identified with job id and model ids

--- exasol_data_science_utils_python/model_utils/udfs/training_runner.py
+++ exasol_data_science_utils_python/model_utils/udfs/training_runner.py
@@ -1,5 +1,5 @@
 from pathlib import PurePosixPath
-from typing import List
+from typing import List, Generator, Any, Dict
 
 import pyexasol
 
@@ -21,6 +21,8 @@ from exasol_data_science_utils_python.udf_utils.sql_executor import SQLExecutor
 
 class TrainingRunner:
     def __init__(self,
+                 job_id: str,
+                 model_id: str,
                  model_connection_object: ConnectionObject,
                  path_under_model_connection: PurePosixPath,
                  db_connection_object: ConnectionObject,
@@ -31,6 +33,8 @@ class TrainingRunner:
                  target_schema: Schema,
                  model,
                  column_preprocessor_creator: AbstractColumnPreprocessorCreator):
+        self.job_id = job_id
+        self.model_id = model_id
         self.path_under_model_connection = path_under_model_connection
         self.source_table = source_table
         self.target_schema = target_schema
@@ -46,7 +50,7 @@ class TrainingRunner:
             raise ValueError(
                 f"Not all columns in {[column.fully_qualified() for column in self.columns]} are from the same table.")
 
-    def run(self):
+    def run(self) -> Dict[str, Any]:
         bucket_fs_factory = BucketFSFactory()
         model_bucketfs_location = \
             bucket_fs_factory.create_bucketfs_location(
@@ -74,6 +78,8 @@ class TrainingRunner:
         sql_udf_stub_creator.create_combine_to_voting_regressor_udf(sql_executor, self.target_schema)
 
         self.run_train_queries(sql_executor)
+        model_info = self.get_combined_model_info(sql_executor)
+        return model_info
 
     def run_train_queries(self,
                           sql_executor: SQLExecutor):
@@ -81,19 +87,49 @@ class TrainingRunner:
         self.run_combine_to_voting_regressor_udf(sql_executor)
 
     def run_combine_to_voting_regressor_udf(self, sql_executor):
-        create_table = f"""
-        CREATE TABLE IF NOT EXISTS {self.target_schema.fully_qualified()}."FITTED_COMBINED_MODEL" (
-            session_id VARCHAR(2000000),
-            model_connection_name VARCHAR(2000000),
-            path_under_model_connection VARCHAR(2000000),
-            model_path VARCHAR(2000000)
-        )
+        self.create_fitted_combined_model_table(sql_executor)
+        self.insert_combined_model(sql_executor)
+
+    def get_combined_model_info(self, sql_executor) -> Dict[str, Any]:
+        result_query = f"""
+            SELECT 
+                job_id,
+                model_id,
+                model_connection_name,
+                path_under_model_connection,
+                model_path
+            FROM {self.target_schema.fully_qualified()}."FITTED_COMBINED_MODEL"
+            WHERE job_id = '{self.job_id}' 
+            AND model_id = '{self.model_id}'
+            AND model_connection_name = '{self.model_connection_object.name}'
+            AND (
+                path_under_model_connection = {self._get_path_under_model_connection_as_sql_value()}
+                OR (
+                    {self._get_path_under_model_connection_as_sql_value()} is null AND
+                    path_under_model_connection is null
+                )
+            )
         """
-        sql_executor.execute(create_table)
+        rs = sql_executor.execute(result_query)
+        rows = rs.fetchall()
+        if rs.rowcount() != 1:
+            raise RuntimeError(f"Internal Error: Got not exactly one model from query {result_query}")
+        row = rows[0]
+        model_info = {
+            "job_id": row[0],
+            "model_id": row[1],
+            "model_connection_name": row[2],
+            "path_under_model_connection": row[3],
+            "model_path": row[4]
+        }
+        return model_info
+
+    def insert_combined_model(self, sql_executor):
         query = f"""
             INSERT INTO {self.target_schema.fully_qualified()}."FITTED_COMBINED_MODEL"
             SELECT  
-                CURRENT_SESSION, 
+                '{self.job_id}',
+                '{self.model_id}',
                 model_connection_name, 
                 path_under_model_connection, 
                 combined_model_path
@@ -103,7 +139,8 @@ class TrainingRunner:
                     path_under_model_connection,
                     model_path) 
                 FROM {self.target_schema.fully_qualified()}."FITTED_BASE_MODELS"
-                WHERE session_id = CURRENT_SESSION
+                WHERE job_id = '{self.job_id}'
+                AND model_id = '{self.model_id}'
                 AND model_connection_name = '{self.model_connection_object.name}'
                 AND (
                     path_under_model_connection = {self._get_path_under_model_connection_as_sql_value()}
@@ -116,6 +153,18 @@ class TrainingRunner:
             """
         sql_executor.execute(query)
 
+    def create_fitted_combined_model_table(self, sql_executor):
+        create_table = f"""
+        CREATE TABLE IF NOT EXISTS {self.target_schema.fully_qualified()}."FITTED_COMBINED_MODEL" (
+            job_id VARCHAR(2000000),
+            model_id VARCHAR(2000000),
+            model_connection_name VARCHAR(2000000),
+            path_under_model_connection VARCHAR(2000000),
+            model_path VARCHAR(2000000)
+        )
+        """
+        sql_executor.execute(create_table)
+
     def _get_path_under_model_connection_as_sql_value(self):
         if self.path_under_model_connection is None:
             return "null"
@@ -127,7 +176,8 @@ class TrainingRunner:
         select_list_for_columns = self.get_select_list_for_columns(self.columns)
         create_table = f"""
         CREATE TABLE IF NOT EXISTS {self.target_schema.fully_qualified()}."FITTED_BASE_MODELS" (
-            session_id VARCHAR(2000000),
+            job_id VARCHAR(2000000),
+            model_id VARCHAR(2000000),
             model_connection_name VARCHAR(2000000),
             path_under_model_connection VARCHAR(2000000),
             model_path VARCHAR(2000000),
@@ -140,7 +190,8 @@ class TrainingRunner:
         query = f"""
             INSERT INTO {self.target_schema.fully_qualified()}."FITTED_BASE_MODELS" 
             SELECT 
-                CURRENT_SESSION, 
+                '{self.job_id}',
+                '{self.model_id}',
                 model_connection_name, 
                 path_under_model_connection, 
                 output_model_path,
