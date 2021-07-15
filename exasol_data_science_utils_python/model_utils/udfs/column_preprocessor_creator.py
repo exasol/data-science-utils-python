43427da7a34a5989de442f18b385fe4830eb205e
Torsten Kilias <torsten.kilias@exasol.com>
2021-07-15 05:25:40 +0200

    Change column_preprocessor_creator.py to use parameter tables.

--- exasol_data_science_utils_python/model_utils/udfs/column_preprocessor_creator.py
+++ exasol_data_science_utils_python/model_utils/udfs/column_preprocessor_creator.py
@@ -8,13 +8,14 @@ from exasol_data_science_utils_python.model_utils.sklearn_contrast_matrix_transf
 from exasol_data_science_utils_python.model_utils.sklearn_min_max_scalar import SKLearnMinMaxScalar
 from exasol_data_science_utils_python.model_utils.udfs.abstract_column_preprocessor_creator import \
     ColumnPreprocssorCreatorResult, AbstractColumnPreprocessorCreator
+from exasol_data_science_utils_python.preprocessing.column_preprocessor_definition import ColumnPreprocessorDefinition
 from exasol_data_science_utils_python.preprocessing.encoding.ordinal_encoder import OrdinalEncoder
 from exasol_data_science_utils_python.preprocessing.normalization.min_max_scaler import MinMaxScaler
+from exasol_data_science_utils_python.preprocessing.parameter_table import ParameterTable
 from exasol_data_science_utils_python.preprocessing.schema.column_name import ColumnName
 from exasol_data_science_utils_python.preprocessing.schema.schema_name import SchemaName
 from exasol_data_science_utils_python.preprocessing.schema.table_name import TableName
 from exasol_data_science_utils_python.preprocessing.table_preprocessor import TablePreprocessor
-from exasol_data_science_utils_python.preprocessing.column_preprocessor_definition import ColumnPreprocessorDefinition
 from exasol_data_science_utils_python.udf_utils.sql_executor import SQLExecutor
 
 
@@ -28,10 +29,11 @@ class ColumnPreprocessorCreator(AbstractColumnPreprocessorCreator):
                                      target_schema: SchemaName) -> ColumnPreprocssorCreatorResult:
         columns = input_columns + target_columns
         column_types = self._get_column_types(columns, source_table, sql_executor)
-        fit_tables = self._fit_table_preprocessor(column_types, columns, source_table, sql_executor, target_schema)
-        input_columns_transformer = self._create_column_transformer(column_types, fit_tables, input_columns,
+        parameter_tables = self._fit_table_preprocessor(column_types, columns, source_table, sql_executor,
+                                                        target_schema)
+        input_columns_transformer = self._create_column_transformer(column_types, parameter_tables, input_columns,
                                                                     source_table, sql_executor)
-        target_column_transformer = self._create_column_transformer(column_types, fit_tables, target_columns,
+        target_column_transformer = self._create_column_transformer(column_types, parameter_tables, target_columns,
                                                                     source_table, sql_executor)
         return ColumnPreprocssorCreatorResult(input_columns_transformer,
                                               target_column_transformer)
@@ -77,7 +79,7 @@ class ColumnPreprocessorCreator(AbstractColumnPreprocessorCreator):
     def _create_column_transformer(
             self,
             column_types: Dict[str, str],
-            fit_tables: List[TableName],
+            parameter_tables: List[ParameterTable],
             columns: List[ColumnName],
             source_table: TableName,
             sql_executor: SQLExecutor):
@@ -85,13 +87,13 @@ class ColumnPreprocessorCreator(AbstractColumnPreprocessorCreator):
         for column in columns:
             column_type = column_types[column.name]
             if column_type == 'DECIMAL(18,0)':
-                transformer = self._create_onehot_transformer(column, fit_tables, sql_executor)
+                transformer = self._create_onehot_transformer(column, parameter_tables, sql_executor)
                 sklearn_transformer.append((column.name, transformer, [column.name]))
             elif column_type.startswith('VARCHAR'):
-                transformer = self._create_onehot_transformer(column, fit_tables, sql_executor)
+                transformer = self._create_onehot_transformer(column, parameter_tables, sql_executor)
                 sklearn_transformer.append((column.name, transformer, [column.name]))
             elif column_type == 'DOUBLE':
-                transformer = self._create_minmax_transformer(column, fit_tables, sql_executor)
+                transformer = self._create_minmax_transformer(column, parameter_tables, sql_executor)
                 sklearn_transformer.append((column.name, transformer, [column.name]))
             else:
                 raise Exception(f"Type '{column_type}' of column {column.fully_qualified()} not supported")
@@ -105,29 +107,32 @@ class ColumnPreprocessorCreator(AbstractColumnPreprocessorCreator):
     def _create_onehot_transformer(
             self,
             column: ColumnName,
-            fit_tables: List[TableName],
+            parameter_tables: List[ParameterTable],
             sql_executor: SQLExecutor):
-        fit_table_for_column = [table for table in fit_tables if f"_{column.name}_" in table.name]
-        if len(fit_table_for_column) != 1:
-            raise Exception(f"Couldn't find fit_table for column '{column.name}'")
+        parameter_table_for_column = self.get_parameter_table_for_column(column, parameter_tables)
         result_set = sql_executor.execute(
-            f"""SELECT "VALUE" FROM {fit_table_for_column[0].fully_qualified()}""")
+            f"""SELECT "VALUE" FROM {parameter_table_for_column.table.name.fully_qualified()}""")
         dictionary = pd.DataFrame(data=result_set.fetchall(), columns=result_set.column_names())
         dictionary = dictionary.set_index("VALUE", drop=False)
         contrast_matrix = pd.get_dummies(dictionary, columns=["VALUE"], drop_first=True)
         transformer = SKLearnContrastMatrixTransformer(contrast_matrix)
         return transformer
 
+    def get_parameter_table_for_column(self, column: ColumnName,
+                                       parameter_tables: List[ParameterTable]) -> ParameterTable:
+        parameter_table_for_column = [table for table in parameter_tables if column.name == table.source_column.name]
+        if len(parameter_table_for_column) != 1:
+            raise Exception(f"Couldn't find parameter table for column '{column.name}'")
+        return parameter_table_for_column[0]
+
     def _create_minmax_transformer(
             self,
             column: ColumnName,
-            fit_tables: List[TableName],
+            parameter_tables: List[ParameterTable],
             sql_executor: SQLExecutor):
-        fit_table_for_column = [table for table in fit_tables if f"_{column.name}_" in table.name]
-        if len(fit_table_for_column) != 1:
-            raise Exception(f"Couldn't find fit_table for column '{column.name}'")
+        parameter_table_for_column = self.get_parameter_table_for_column(column, parameter_tables)
         result_set = sql_executor.execute(
-            f"""SELECT "MIN", "RANGE"  FROM {fit_table_for_column[0].fully_qualified()}""")
+            f"""SELECT "MIN", "RANGE"  FROM {parameter_table_for_column.table.name.fully_qualified()}""")
         rows = result_set.fetchall()
         min_value = rows[0][0]
         range_value = rows[0][1]
