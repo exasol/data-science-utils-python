acf9a2e3376734fc8967b94ad275ac12a3eb93db
Torsten Kilias <torsten.kilias@exasol.com>
2021-07-15 19:36:36 +0200

    Changed SKLearnPrefittedOneHotTransformer from using a generated contrast matrix to using the method to generate the contrast matrix for transforming data - This saves us memory, because we don't need to store the contrast matrix, but it might be a bit slower. However, memory is more important, because this limits the number of parallel instances.

--- exasol_data_science_utils_python/preprocessing/scikit_learn/sklearn_prefitted_one_hot_transformer.py
+++ exasol_data_science_utils_python/preprocessing/scikit_learn/sklearn_prefitted_one_hot_transformer.py
@@ -1,40 +1,30 @@
 import numpy as np
 import pandas as pd
 
-from exasol_data_science_utils_python.preprocessing.scikit_learn.sklearn_prefitted_transformer import SKLearnPrefittedTransformer
+from exasol_data_science_utils_python.preprocessing.scikit_learn.sklearn_prefitted_transformer import \
+    SKLearnPrefittedTransformer
 
 
 class SKLearnPrefittedOneHotTransformer(SKLearnPrefittedTransformer):
     def __init__(self, dictionary: pd.DataFrame):
-        self._dictionary = dictionary
-        columns_set = set(self._dictionary.columns.to_list())
+        self.dictionary = dictionary.set_index("VALUE", drop=False)
+        columns_set = set(self.dictionary.columns.to_list())
         expected_column_set = {"ID", "VALUE"}
         if columns_set != expected_column_set:
             raise Exception(f"Columns of dictionary {columns_set} don't fit {expected_column_set}")
-        self._contrast_matrix = None
-
-    def __getstate__(self):
-        copy = dict(self.__dict__)
-        copy["_contrast_matrix"] = None
-        return copy
 
     def transform(self, X, y=None):
-        if self._contrast_matrix is None:
-            self._contrast_matrix = self.create_contrast_matrix()
-
+        if not (len(X.shape) == 1 or len(X.shape) == 2 and X.shape == 1):
+            raise ValueError(f"X {X} can't be interpreted as column vector")
         reshape_X = X.values.reshape((-1,))
-        X_isin_index = self._dictionary.index.isin(reshape_X)
-        if all(X_isin_index):
+        setdiff_X_dictionary = np.setdiff1d(reshape_X, self.dictionary.index.values)
+        if len(setdiff_X_dictionary) > 0:
             raise ValueError(
-                f"The following values {reshape_X[np.logical_not(X_isin_index)]} in X are not in the dictionary")
-        X_ = self._contrast_matrix.loc[reshape_X].values
-        return X_
-
-    def create_contrast_matrix(self):
-        dictionary_size = len(self._dictionary)
-        contrast_matrix = np.zeros(shape=(dictionary_size, dictionary_size))
-        for row, column in enumerate(self._dictionary["ID"]):
-            contrast_matrix[row, column] = 1
-        contrast_matrix = contrast_matrix[:,1:]
-        contrast_matrix_df = pd.DataFrame(data=contrast_matrix, index=self._dictionary["VALUE"])
-        return contrast_matrix_df
+                f"The following values {setdiff_X_dictionary} in X are not in the dictionary")
+        dictionary_size = len(self.dictionary)
+        X_size = len(reshape_X)
+        result_X = np.zeros(shape=(X_size, dictionary_size))
+        for row, value in enumerate(reshape_X):
+            result_X[row, self.dictionary.at[value, "ID"]] = 1
+        result_X = result_X[:, 1:]
+        return result_X
--- tests/unit_tests/preprocessing/scikit_learn/test_sklearn_one_hot_transformer.py
+++ tests/unit_tests/preprocessing/scikit_learn/test_sklearn_one_hot_transformer.py
@@ -1,33 +1,11 @@
+import timeit
+
 import numpy as np
 import pandas as pd
 
 from exasol_data_science_utils_python.preprocessing.scikit_learn.sklearn_prefitted_one_hot_transformer import \
     SKLearnPrefittedOneHotTransformer
 
-
-def test_create_contrast_matrix():
-    dictionary = pd.DataFrame(data=[
-        ["A", 1],
-        ["B", 0],
-        ["C", 2],
-        ["D", 3],
-        ["E", 4]], columns=["VALUE", "ID"])
-    transformer = SKLearnPrefittedOneHotTransformer(dictionary)
-    contrast_matrix = transformer.create_contrast_matrix()
-    expected_contrast_matrix = np.array(
-        [
-            [1., 0., 0., 0.],
-            [0., 0., 0., 0.],
-            [0., 1., 0., 0.],
-            [0., 0., 1., 0.],
-            [0., 0., 0., 1.],
-        ])
-    print(expected_contrast_matrix)
-    print(contrast_matrix.index)
-    print(contrast_matrix.values)
-    print(contrast_matrix)
-    assert contrast_matrix.index.to_list() == ["A","B","C","D","E"]
-    assert np.array_equal(expected_contrast_matrix, contrast_matrix.values)
 
 def test_transform():
     dictionary = pd.DataFrame(data=[
@@ -55,3 +33,18 @@ def test_transform():
     print(expected_X_)
     assert np.array_equal(X_, expected_X_)
 
+
+def test_transform_large():
+    dictionary = pd.DataFrame(data=[
+        [f"{i}", i] for i in range(1000)
+    ], columns=["VALUE", "ID"])
+    transformer = SKLearnPrefittedOneHotTransformer(dictionary)
+    X = pd.DataFrame(data=[
+        [f"{i}", ] for i in range(1000)
+    ], columns=["C1"])
+
+    def run():
+        X_ = transformer.transform(X["C1"])
+
+    timer = timeit.timeit(run, number=100)
+    print(timer / 100)
