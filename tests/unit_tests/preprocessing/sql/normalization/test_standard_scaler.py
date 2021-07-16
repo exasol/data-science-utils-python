6979c21d35c6209fb5c6f433ecce449423b3264a
Torsten Kilias <torsten.kilias@exasol.com>
2021-07-16 12:26:08 +0200

    Add SQLStandardScaler - and fix typos

--- tests/unit_tests/preprocessing/sql/normalization/test_standard_scaler.py
+++ tests/unit_tests/preprocessing/sql/normalization/test_standard_scaler.py
@@ -1,6 +1,6 @@
 import textwrap
 
-from exasol_data_science_utils_python.preprocessing.sql.normalization.sql_min_max_scaler import SQLMinMaxScaler
+from exasol_data_science_utils_python.preprocessing.sql.normalization.sql_standard_scaler import SQLStandardScaler
 from exasol_data_science_utils_python.preprocessing.sql.parameter_table import ParameterTable
 from exasol_data_science_utils_python.preprocessing.sql.schema.column import Column
 from exasol_data_science_utils_python.preprocessing.sql.schema.column_name import ColumnName
@@ -13,23 +13,23 @@ from exasol_data_science_utils_python.preprocessing.sql.transformation_column im
 from tests.unit_tests.mock_sql_executor import MockSQLExecutor
 
 
-def test_min_max_scaler_create_fit_queries():
+def test_standard_scaler_create_fit_queries():
     source_schema = SchemaName("SRC_SCHEMA")
     source_table = TableName("SRC_TABLE", source_schema)
     target_schema = SchemaName("TGT_SCHEMA")
     source_column = ColumnName("SRC_COLUMN1", source_table)
     experiment_name = ExperimentName("EXPERIMENT")
-    scaler = SQLMinMaxScaler()
+    scaler = SQLStandardScaler()
     mock_sql_executor = MockSQLExecutor()
     parameter_tables = scaler.fit(mock_sql_executor, source_column, target_schema, experiment_name)
 
     expected_parameter_tables = get_expected_parameter_tables()
 
     expected = textwrap.dedent("""
-        CREATE OR REPLACE TABLE "TGT_SCHEMA"."EXPERIMENT_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_MIN_MAX_SCALER_PARAMETERS" AS
+        CREATE OR REPLACE TABLE "TGT_SCHEMA"."EXPERIMENT_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS" AS
         SELECT
-            CAST(MIN("SRC_SCHEMA"."SRC_TABLE"."SRC_COLUMN1") as DOUBLE) as "MIN",
-            CAST(MAX("SRC_SCHEMA"."SRC_TABLE"."SRC_COLUMN1")-MIN("SRC_SCHEMA"."SRC_TABLE"."SRC_COLUMN1") as DOUBLE) as "RANGE"
+            CAST(AVG("SRC_SCHEMA"."SRC_TABLE"."SRC_COLUMN1") as DOUBLE) as "AVG",
+            CAST(STDDEV("SRC_SCHEMA"."SRC_TABLE"."SRC_COLUMN1") as DOUBLE) as "STDDEV"
         FROM "SRC_SCHEMA"."SRC_TABLE"
         """)
     assert mock_sql_executor.queries == [expected]
@@ -37,22 +37,22 @@ def test_min_max_scaler_create_fit_queries():
 
 
 def get_expected_parameter_tables():
-    target_table_name = TableName("EXPERIMENT_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_MIN_MAX_SCALER_PARAMETERS",
+    target_table_name = TableName("EXPERIMENT_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS",
                                   SchemaName("TGT_SCHEMA"))
     expected_parameter_table = ParameterTable(
         source_column=ColumnName("SRC_COLUMN1", TableName("SRC_TABLE", SchemaName("SRC_SCHEMA"))),
         table=Table(target_table_name,
                     columns=
                     [
-                        Column(ColumnName("MIN", target_table_name), ColumnType("DOUBLE")),
-                        Column(ColumnName("RANGE", target_table_name), ColumnType("DOUBLE"))
+                        Column(ColumnName("AVG", target_table_name), ColumnType("DOUBLE")),
+                        Column(ColumnName("STDDEV", target_table_name), ColumnType("DOUBLE"))
                     ]),
-        purpose="MIN_AND_RANGE_TABLE"
+        purpose="AVG_AND_STDDEV_TABLE"
     )
     return [expected_parameter_table]
 
 
-def test_min_max_scaler_create_from_clause_part():
+def test_standard_scaler_create_from_clause_part():
     source_schema = SchemaName("SRC_SCHEMA")
     source_table = TableName("SRC_TABLE", source_schema)
     target_schema = SchemaName("TGT_SCHEMA")
@@ -60,15 +60,15 @@ def test_min_max_scaler_create_from_clause_part():
     input_schema = SchemaName("IN_SCHEMA")
     input_table = TableName("IN_TABLE", input_schema)
     experiment_name = ExperimentName("EXPERIMENT")
-    scaler = SQLMinMaxScaler()
+    scaler = SQLStandardScaler()
     mock_sql_executor = MockSQLExecutor()
     from_clause_part = scaler.create_transform_from_clause_part(
         mock_sql_executor, source_column, input_table, target_schema, experiment_name)
     assert from_clause_part[0] == textwrap.dedent(
-        f'''CROSS JOIN "TGT_SCHEMA"."EXPERIMENT_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_MIN_MAX_SCALER_PARAMETERS" AS "TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_MIN_MAX_SCALER_PARAMETERS"''')
+        f'''CROSS JOIN "TGT_SCHEMA"."EXPERIMENT_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS" AS "TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS"''')
 
 
-def test_min_max_scaler_create_select_clause_part():
+def test_standard_scaler_create_select_clause_part():
     source_schema = SchemaName("SRC_SCHEMA")
     source_table = TableName("SRC_TABLE", source_schema)
     target_schema = SchemaName("TGT_SCHEMA")
@@ -76,18 +76,23 @@ def test_min_max_scaler_create_select_clause_part():
     input_schema = SchemaName("IN_SCHEMA")
     input_table = TableName("IN_TABLE", input_schema)
     experiment_name = ExperimentName("EXPERIMENT")
-    scaler = SQLMinMaxScaler()
+    scaler = SQLStandardScaler()
     mock_sql_executor = MockSQLExecutor()
     select_clause_parts = scaler.create_transform_select_clause_part(
         mock_sql_executor, source_column, input_table, target_schema, experiment_name)
     expected = textwrap.dedent(
-        '''("IN_SCHEMA"."IN_TABLE"."SRC_COLUMN1"-"TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_MIN_MAX_SCALER_PARAMETERS"."MIN")/"TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_MIN_MAX_SCALER_PARAMETERS"."RANGE" AS "SRC_COLUMN1_MIN_MAX_SCALED"''')
+        '''("IN_SCHEMA"."IN_TABLE"."SRC_COLUMN1"-"TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS"."AVG")/
+        (CASE 
+        WHEN "TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS"."STDDEV" = 0 THEN 1
+        ELSE "TGT_SCHEMA_SRC_SCHEMA_SRC_TABLE_SRC_COLUMN1_STANDARD_SCALER_PARAMETERS"."STDDEV"
+        END)
+        AS "SRC_COLUMN1_STANDARD_SCALED"''')
     expected_transformation_column = \
         TransformationColumn(
             source_column=ColumnName("SRC_COLUMN1", source_table),
             input_column=ColumnName("SRC_COLUMN1", input_table),
-            purpose="MinMaxScaled",
-            column=Column(ColumnName("SRC_COLUMN1_MIN_MAX_SCALED"), ColumnType("DOUBLE"))
+            purpose="StandardScaled",
+            column=Column(ColumnName("SRC_COLUMN1_STANDARD_SCALED"), ColumnType("DOUBLE"))
         )
     assert len(select_clause_parts) == 1
     assert select_clause_parts[0].select_clause_part_expression == expected
